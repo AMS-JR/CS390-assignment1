f72e41926e6226ec08824423d16438d44b861e58
Amadou Sarjo Jallow <asj.sarjo@gmail.com>
2024-11-25 18:18:32 -0600

    commit again

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = {1, 2, 3, 4, 5};
+        int[] numbers = {1, 2, 3, 4, 5, 7};
         System.out.println("String elements: " + java.util.Arrays.toString(numbers));
     }
 }
