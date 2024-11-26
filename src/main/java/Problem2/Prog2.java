6c1cf6b48660f9d30115eb841c682da30dfe84c4
Kisekka-Alexander <kkalexander717@gmail.com>
2024-11-26 14:24:14 -0600

    Problem 2

---
+++ src/main/java/Problem2/Prog2.java
@@ -0,0 +1,15 @@
+package Problem2;
+
+public class Prog2 {
+
+    public static void main(String [] args){
+        float f1 = 1.27f, f2 = 3.881f, f3 = 9.6f;
+
+        int sum = (int)(f1+f2+f3);
+        System.out.println(sum);
+
+        int round_sum = Math.round(f1+f2+f3);
+        System.out.println(round_sum);
+
+    }
+}
