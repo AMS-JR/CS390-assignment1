e1273ed9b067634fc0b14d4416b2448ad9179142
Amadou Sarjo Jallow <asj.sarjo@gmail.com>
2024-11-26 07:38:51 -0600

    commit Prog1

---
+++ src/main/java/problem1/Prog1.java
@@ -0,0 +1,19 @@
+package problem1;
+
+import java.util.random.RandomGenerator;
+
+public class Prog1 {
+    public static void main(String[] args) {
+        RandomGenerator rand = RandomGenerator.getDefault();
+        // Generate a random integer between 1 and 9(inclusive) and calculate the power of PI to that number.
+        int x = rand.nextInt(1,10);
+        // Generate a random integer between 3 and 14(inclusive) and calculate the power of PI to that number.
+        int y = rand.nextInt(3,15);
+        System.out.println("Random integer in range 1 .. 9: " + x);
+        System.out.printf("π^x: %.2f%n", Math.pow(Math.PI, x));
+
+        System.out.println("Random integer in range 3 .. 14: " + y);
+        System.out.printf("y^π: %.2f%n", Math.pow(y, Math.PI));
+
+    }
+}
